fa30ae8c6a5c4e3512f6ed93e1300a1d26532806
Denitsa <denitsabebrevenska@gmail.com>
2024-09-22 10:53:46 +0300

    added references and packages, added swashbuckle`s config in program.cs

--- StirCraftApp.Api/Program.cs
+++ StirCraftApp.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Title = "StirCraft API",
+		Version = "v1"
+	});
+});
 
 var app = builder.Build();
 
@@ -13,7 +22,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("v1/swagger.json", "StirCraft API V1");
+	});
 }
 
 app.UseHttpsRedirection();
