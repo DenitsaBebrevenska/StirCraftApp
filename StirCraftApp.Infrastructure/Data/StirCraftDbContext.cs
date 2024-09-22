a07ee54e13675e105146af616f0fec90092f7afb
Denitsa <denitsabebrevenska@gmail.com>
2024-09-22 15:47:32 +0300

    added db sets

---
+++ StirCraftApp.Infrastructure/Data/StirCraftDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StirCraftApp.Domain.Entities;
+
+namespace StirCraftApp.Infrastructure.Data;
+public class StirCraftDbContext : IdentityDbContext<IdentityUser>
+{
+	public DbSet<Category> Categories { get; set; }
+
+	public DbSet<Comment> Comments { get; set; }
+
+	public DbSet<Cook> Cooks { get; set; }
+
+	public DbSet<Ingredient> Ingredients { get; set; }
+
+	public DbSet<MeasurementUnit> MeasurementUnits { get; set; }
+
+	public DbSet<Recipe> Recipes { get; set; }
+
+	public DbSet<RecipeImage> RecipeImages { get; set; }
+
+	public DbSet<RecipeRating> RecipeRatings { get; set; }
+
+	public DbSet<Reply> Replies { get; set; }
+
+	public DbSet<User> ApplicationUsers { get; set; }
+	public StirCraftDbContext(DbContextOptions<StirCraftDbContext> options) : base(options)
+	{
+	}
+
+}
