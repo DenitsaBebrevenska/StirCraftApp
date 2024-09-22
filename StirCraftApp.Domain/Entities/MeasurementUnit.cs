3fccfec9735340367497f650bc00012495fc2e89
Denitsa <denitsabebrevenska@gmail.com>
2024-09-22 15:46:12 +0300

    added measurement unit as entity

---
+++ StirCraftApp.Domain/Entities/MeasurementUnit.cs
@@ -0,0 +1,9 @@
+﻿namespace StirCraftApp.Domain.Entities;
+public class MeasurementUnit
+{
+	public int Id { get; set; }
+
+	public required string Name { get; set; }
+
+	public required string Abbreviation { get; set; }
+}
