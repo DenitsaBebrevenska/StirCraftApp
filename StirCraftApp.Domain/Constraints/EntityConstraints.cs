8ab5a186428f99603104b54a556237fd428d3fc9
Denitsa <denitsabebrevenska@gmail.com>
2024-09-22 15:47:17 +0300

    All entity constraints added for now in Domain layer, could be relocated

---
+++ StirCraftApp.Domain/Constraints/EntityConstraints.cs
@@ -0,0 +1,37 @@
+﻿namespace StirCraftApp.Domain.Constraints;
+public static class EntityConstraints
+{
+	//not sure if those will stay in domain...
+	//Comment
+	public const int CommentTitleMinLength = 2;
+	public const int CommentTitleMaxLength = 20;
+	public const int CommentBodyMinLength = 2;
+	public const int CommentBodyMaxLength = 500;
+
+	//Reply
+	public const int ReplyBodyMinLength = 2;
+	public const int ReplyBodyMaxLength = 250;
+
+	//Cook
+	public const int CookAboutMinLength = 10;
+	public const int CookAboutMaxLength = 200;
+
+	//Ingredient
+	public const int IngredientNameMinLength = 2;
+	public const int IngredientNameMaxLength = 100;
+
+	//Recipe
+	public const int RecipeNameMinLength = 2;
+	public const int RecipeNameMaxLength = 100;
+	public const int RecipeDescriptionMinLength = 50;
+	public const int RecipeDescriptionMaxLength = 2_000;
+
+	//Image that will stay if is in user control
+	public const int ImageUrlMaxLength = 2_048;
+
+	//Unit
+	public const int UnitNameMinLength = 2;
+	public const int UnitNameMaxLength = 20;
+	public const int UnitAbbreviationMinLength = 1;
+	public const int UnitAbbreviationMaxLength = 5;
+}
