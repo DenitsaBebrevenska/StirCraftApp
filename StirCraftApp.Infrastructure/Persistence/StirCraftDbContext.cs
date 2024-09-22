387fa3fe174f039ffda611459afb66e0711d2029
Denitsa <denitsabebrevenska@gmail.com>
2024-09-22 15:46:32 +0300

    added context to services

--- StirCraftApp.Infrastructure/Persistence/StirCraftDbContext.cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace StirCraftApp.Infrastructure.Persistence;
-public class StirCraftDbContext(DbContextOptions<StirCraftDbContext> options)
-	: IdentityDbContext<ApplicationUser>(options);
