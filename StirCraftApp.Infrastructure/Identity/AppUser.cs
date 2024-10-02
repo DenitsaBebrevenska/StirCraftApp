d6b89bc968716cc333892aa9a862f3bf30ec3a9b
Denitsa <denitsabebrevenska@gmail.com>
2024-10-02 10:58:35 +0300

    AppUser now extends Identity User

---
+++ StirCraftApp.Infrastructure/Identity/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using StirCraftApp.Domain.Entities;
+
+namespace StirCraftApp.Infrastructure.Identity;
+public class AppUser : IdentityUser
+{
+	public string? AvatarUrl { get; set; }
+
+	public virtual ICollection<Recipe> FavoriteRecipes { get; set; } = new List<Recipe>();
+
+	public virtual ICollection<ShoppingList> ShoppingLists { get; set; } = new List<ShoppingList>();
+
+	public virtual ICollection<RecipeRating> RecipesRatings { get; set; } = new List<RecipeRating>();
+}
