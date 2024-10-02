21fd754691a72915c75b7a310a1dac7d4674daca
Denitsa <denitsabebrevenska@gmail.com>
2024-10-02 10:58:55 +0300

    db connection extension added

---
+++ StirCraftApp.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using StirCraftApp.Infrastructure.Data;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration config)
+	{
+		var connectionString = config.GetConnectionString("DefaultConnection") ??
+							   throw new ArgumentException("DefaultConnection string not set.");
+
+		services.AddDbContext<StirCraftDbContext>(options => options.UseSqlServer(connectionString));
+
+		return services;
+	}
+}
