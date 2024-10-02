6df2d20fadb34c0bbcfb8c0a16d50d4e8c12496f
Denitsa <denitsabebrevenska@gmail.com>
2024-10-02 14:58:54 +0300

    added soft delete filter with questionable reflexion. Needs testing!

--- StirCraftApp.Infrastructure/Data/StirCraftDbContext.cs
+++ StirCraftApp.Infrastructure/Data/StirCraftDbContext.cs
@@ -1,32 +1,84 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StirCraftApp.Domain.Contracts;
 using StirCraftApp.Domain.Entities;
+using StirCraftApp.Infrastructure.Identity;
+using System.Reflection;
 
 namespace StirCraftApp.Infrastructure.Data;
-public class StirCraftDbContext : IdentityDbContext<IdentityUser>
+public class StirCraftDbContext(DbContextOptions<StirCraftDbContext> options) : IdentityDbContext<AppUser>(options)
 {
-	public DbSet<Category> Categories { get; set; }
+	public DbSet<Category> Categories { get; set; } = null!;
 
-	public DbSet<Comment> Comments { get; set; }
+	public DbSet<Comment> Comments { get; set; } = null!;
 
-	public DbSet<Cook> Cooks { get; set; }
+	public DbSet<Cook> Cooks { get; set; } = null!;
 
-	public DbSet<Ingredient> Ingredients { get; set; }
+	public DbSet<Ingredient> Ingredients { get; set; } = null!;
 
-	public DbSet<MeasurementUnit> MeasurementUnits { get; set; }
+	public DbSet<RecipeIngredient> RecipeIngredients { get; set; } = null!;
 
-	public DbSet<Recipe> Recipes { get; set; }
+	public DbSet<MeasurementUnit> MeasurementUnits { get; set; } = null!;
 
-	public DbSet<RecipeImage> RecipeImages { get; set; }
+	public DbSet<Recipe> Recipes { get; set; } = null!;
 
-	public DbSet<RecipeRating> RecipeRatings { get; set; }
+	public DbSet<RecipeImage> RecipeImages { get; set; } = null!;
 
-	public DbSet<Reply> Replies { get; set; }
+	public DbSet<RecipeRating> RecipeRatings { get; set; } = null!;
 
-	public DbSet<User> ApplicationUsers { get; set; }
-	public StirCraftDbContext(DbContextOptions<StirCraftDbContext> options) : base(options)
+	public DbSet<Reply> Replies { get; set; } = null!;
+
+	protected override void OnModelCreating(ModelBuilder builder)
 	{
+		builder.Entity<Cook>()
+			.HasOne<AppUser>()
+			.WithOne()
+			.HasForeignKey<Cook>(c => c.UserId);
+
+		builder.Entity<Comment>()
+			.HasOne<AppUser>()
+			.WithOne()
+			.HasForeignKey<Comment>(c => c.UserId);
+
+		builder.Entity<Reply>()
+			.HasOne<AppUser>()
+			.WithOne()
+			.HasForeignKey<Reply>(r => r.UserId);
+
+		builder.Entity<ShoppingList>()
+			.HasOne<AppUser>()
+			.WithOne()
+			.HasForeignKey<ShoppingList>(s => s.UserId);
+
+		builder.Entity<RecipeRating>()
+			.HasOne<AppUser>()
+			.WithOne()
+			.HasForeignKey<RecipeRating>(rr => rr.UserId);
+
+		var softDeletables = builder
+			.Model
+			.GetEntityTypes()
+			.Where(et => typeof(ISoftDeletable).IsAssignableFrom(et.ClrType));
+
+		foreach (var entityType in softDeletables)
+		{
+			var method = typeof(StirCraftDbContext)
+				.GetMethod(nameof(ApplySoftDeleteFilter), BindingFlags.NonPublic | BindingFlags.Static)?
+				.MakeGenericMethod(entityType.ClrType);
+
+			method?.Invoke(null, new object[] { builder });
+		}
+
+
+		base.OnModelCreating(builder);
+
+	}
+
+
+	private static void ApplySoftDeleteFilter<TEntity>(ModelBuilder builder) where TEntity : class, ISoftDeletable
+	{
+		builder.Entity<TEntity>()
+			.HasQueryFilter(e => !e.IsDeleted);
 	}
 
 }
