9495a6356d6fef122571a039488161fab7a4b300
Denitsa <denitsabebrevenska@gmail.com>
2024-10-06 16:43:44 +0300

    implemented one controller method just to test out how things are and if API actually works

---
+++ StirCraftApp.Api/Controllers/RecipesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StirCraftApp.Domain.Contracts;
+using StirCraftApp.Domain.Entities;
+
+namespace StirCraftApp.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class RecipesController : ControllerBase
+{
+	private readonly IRepository<Recipe> _recipeRepository;
+	//use repo for now to test out
+	public RecipesController(IRepository<Recipe> recipeRepository)
+	{
+		_recipeRepository = recipeRepository;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetRecipes()
+	{
+		var recipes = await _recipeRepository
+			.GetAllAsync();
+
+		return Ok(recipes);
+	}
+}
