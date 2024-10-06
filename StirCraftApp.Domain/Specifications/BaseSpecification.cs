1ac358c4200c2aa33d7a082b5127d2486dd01a0b
Denitsa <denitsabebrevenska@gmail.com>
2024-10-06 15:04:22 +0300

    base spec added

---
+++ StirCraftApp.Domain/Specifications/BaseSpecification.cs
@@ -0,0 +1,9 @@
+﻿using StirCraftApp.Domain.Contracts;
+using System.Linq.Expressions;
+
+namespace StirCraftApp.Domain.Specifications;
+public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
+{
+	protected BaseSpecification() : this(null) { }
+	public Expression<Func<T, bool>>? Criteria => criteria;
+}
---
+++ StirCraftApp.Domain/Specifications/RecipeSpecification.cs
@@ -0,0 +1,11 @@
+﻿using StirCraftApp.Domain.Entities;
+
+namespace StirCraftApp.Domain.Specifications;
+public class RecipeSpecification : BaseSpecification<Recipe>
+{
+	public RecipeSpecification(string? name, string? category, string? difficultyLevel)
+	: base()
+	{
+		//todo continue working on that as is not implemented at all
+	}
+}
