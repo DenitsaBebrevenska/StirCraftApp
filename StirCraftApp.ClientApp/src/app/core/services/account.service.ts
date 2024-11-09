542dc8a85e2edb545b3d7010837c7bee76483a5e
Denitsa <denitsabebrevenska@gmail.com>
2024-11-09 18:05:43 +0200

    added environments and some changes to account serivce and header

--- StirCraftApp.ClientApp/src/app/core/services/account.service.ts
+++ StirCraftApp.ClientApp/src/app/core/services/account.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../shared/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AccountService {
-  
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
+
+  /* login(values: any) {
+    return this.http.post(this.baseUrl + 'account/login', values).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUser.next(user);
+        }
+      })
+    );
+  } */
 }
--- StirCraftApp.ClientApp/src/app/layout/header/header.component.ts
+++ StirCraftApp.ClientApp/src/app/layout/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button'
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,8 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   imports: [
     RouterLink,
     RouterLinkActive,
-    MatButton
+    MatButton,
+    MatProgressBar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
