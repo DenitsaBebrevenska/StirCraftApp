2866088409fbd8e5c2a743e4267de4267cce8f36
Denitsa <denitsabebrevenska@gmail.com>
2024-11-20 23:49:19 +0200

    repository get method now can use spec or set it as null and get all without spec

--- StirCraftApp.Domain/Contracts/IRepository.cs
+++ StirCraftApp.Domain/Contracts/IRepository.cs
@@ -3,12 +3,12 @@
 namespace StirCraftApp.Domain.Contracts;
 public interface IRepository<T> where T : BaseEntity
 {
-    Task<T?> GetByIdAsync(int id);
-    Task<T?> GetEntityWithSpecAsync(ISpecification<T> spec, int id);
-    Task<IList<T>> GetAllAsync();
-    Task<IList<T>> GetAllWithSpecAsync(ISpecification<T> spec);
+    //Task<T?> GetByIdAsync(int id);
+    Task<T?> GetByIdAsync(ISpecification<T>? spec, int id);
+    Task<IList<T>> GetAllAsync(ISpecification<T>? spec);
+    //Task<IList<T>> GetAllAsync();
     //Task<TResult?> GetEntityWithSpecAsync<TResult>(ISpecification<T, TResult> spec);
-    //Task<IList<TResult>> GetAllWithSpecAsync<TResult>(ISpecification<T, TResult> spec);
+    //Task<IList<TResult>> GetAllAsync<TResult>(ISpecification<T, TResult> spec);
     Task AddAsync(T entity);
     void Delete(T entity);
     void Update(T entity);
--- StirCraftApp.Infrastructure/Data/Repository.cs
+++ StirCraftApp.Infrastructure/Data/Repository.cs
@@ -9,27 +9,43 @@ public class Repository<T>(StirCraftDbContext context) : IRepository<T>
     private DbSet<T> GetDbSet()
         => context.Set<T>();
 
-    public async Task<T?> GetByIdAsync(int id)
-        => await GetDbSet()
-            .FindAsync(id);
+    //public async Task<T?> GetByIdAsync(int id)
+    //    => await GetDbSet()
+    //        .FindAsync(id);
 
-    public async Task<T?> GetEntityWithSpecAsync(ISpecification<T> spec, int id)
-         => await ApplySpecification(spec)
-            .FirstOrDefaultAsync(x => x.Id == id);
+    public async Task<T?> GetByIdAsync(ISpecification<T>? spec, int id)
+    {
+        if (spec == null)
+        {
+            return await GetDbSet()
+                .FindAsync(id); ;
+        }
 
-    public async Task<IList<T>> GetAllAsync()
-        => await GetDbSet()
+        return await ApplySpecification(spec)
+            .FirstOrDefaultAsync(x => x.Id == id); ;
+    }
+
+    //public async Task<IList<T>> GetAllAsync()
+    //    => await GetDbSet()
+    //        .ToListAsync();
+
+    public async Task<IList<T>> GetAllAsync(ISpecification<T>? spec)
+    {
+        if (spec == null)
+        {
+            return await GetDbSet()
+                .ToListAsync();
+        }
+
+        return await ApplySpecification(spec)
             .ToListAsync();
-
-    public async Task<IList<T>> GetAllWithSpecAsync(ISpecification<T> spec)
-        => await ApplySpecification(spec)
-            .ToListAsync();
+    }
 
     //public async Task<TResult?> GetEntityWithSpecAsync<TResult>(ISpecification<T, TResult> spec)
     // => await ApplySpecification(spec)
     //     .FirstOrDefaultAsync();
 
-    //public async Task<IList<TResult>> GetAllWithSpecAsync<TResult>(ISpecification<T, TResult> spec)
+    //public async Task<IList<TResult>> GetAllAsync<TResult>(ISpecification<T, TResult> spec)
     //    => await ApplySpecification(spec)
     //        .ToListAsync();
 
@@ -55,12 +71,11 @@ public class Repository<T>(StirCraftDbContext context) : IRepository<T>
         return entity is not null;
     }
 
-    //todo check if no tracking will cause any problem later on
+
     private IQueryable<T> ApplySpecification(ISpecification<T> spec)
     {
         var query = GetDbSet()
-            .AsQueryable()
-            .AsNoTracking();
+            .AsQueryable();
         return SpecificationEvaluator<T>.GetQuery(query, spec);
     }
 
